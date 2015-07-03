4fd8a582218e002a6cbf6a7483bd74adba73b838
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-07-03 13:17:57 +0530

    Added support files

---
+++ src/com/wizrocket/plugin/GenerateAction.java
@@ -0,0 +1,13 @@
+package com.wizrocket.plugin;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by VardhmanMehta on 03/07/15.
+ */
+public class  GenerateAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}

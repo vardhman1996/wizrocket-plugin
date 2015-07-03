a44994788e633af88dd38ee44a8ad63fb458513a
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-07-03 14:36:47 +0530

    Added document listner

--- src/com/wizrocket/plugin/GenerateAction.java
+++ src/com/wizrocket/plugin/GenerateAction.java
@@ -3,7 +3,10 @@ package com.wizrocket.plugin;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.event.DocumentEvent;
+import com.intellij.openapi.editor.event.DocumentListener;
 
 /**
  * Created by VardhmanMehta on 03/07/15.
@@ -15,7 +18,19 @@ public class  GenerateAction extends AnAction {
 
     public void actionPerformed(AnActionEvent event) {
         Editor editor = DataKeys.EDITOR.getData(event.getDataContext());
+        DocumentProvider document = new DocumentProvider(editor);
+        final Document doc = document.getDoc();
+        doc.addDocumentListener(new DocumentListener() {
+            @Override
+            public void beforeDocumentChange(DocumentEvent documentEvent) {
 
+            }
+
+            @Override
+            public void documentChanged(DocumentEvent documentEvent) {
+                
+            }
+        });
 
     }
 }

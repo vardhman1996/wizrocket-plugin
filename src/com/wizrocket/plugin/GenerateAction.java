e84b968c2eb6ffd0d06c372b7898fb924205d796
Vardhman Mehta <vardhman.mehta96@gamil.com>
2015-07-08 16:38:44 +0530

    Added in-app notification validations

--- src/com/wizrocket/plugin/GenerateAction.java
+++ src/com/wizrocket/plugin/GenerateAction.java
@@ -81,6 +81,7 @@ public class GenerateAction extends AnAction {
 
             validateGcmReceiver(children);
 
+            validateInAppNotifications(children);
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
@@ -118,17 +119,17 @@ public class GenerateAction extends AnAction {
         } else {
             logger.info("Does not contain required uses permissions");
         }
-        if(!usesSet.contains("android.permission.ACCESS_NETWORK_STATE")||
-            !usesSet.contains("android.permission.GET_ACCOUNTS")||
-            !usesSet.contains("android.permission.ACCESS_COARSE_LOCATION")||
-            !usesSet.contains("android.permission.WRITE_EXTERNAL_STORAGE")) {
+        if (!usesSet.contains("android.permission.ACCESS_NETWORK_STATE") ||
+                !usesSet.contains("android.permission.GET_ACCOUNTS") ||
+                !usesSet.contains("android.permission.ACCESS_COARSE_LOCATION") ||
+                !usesSet.contains("android.permission.WRITE_EXTERNAL_STORAGE")) {
 
             logger.info("Does not contain recommended uses permissions");
         } else {
             logger.info("Does contain recommended uses permissions");
         }
 
-        if(usesSet.contains("signature") && usesSet.contains(userPackage + ".permission.C2D_MESSAGE") && usesSet.contains("com.google.android.c2dm.permission.RECEIVE")) {
+        if (usesSet.contains("signature") && usesSet.contains(userPackage + ".permission.C2D_MESSAGE") && usesSet.contains("com.google.android.c2dm.permission.RECEIVE")) {
             logger.info("GCM uses permissions configured correctly");
         } else {
             logger.info("GCM uses permissions not used yet");
@@ -139,79 +140,86 @@ public class GenerateAction extends AnAction {
         boolean one = false;
         boolean two = false;
         int length = children.getLength();
-        for (int i = 0; i<length; i++) {
+        for (int i = 0; i < length; i++) {
             Node item = children.item(i);
             Matcher m;
             if (!item.getNodeName().equals("meta-data")) continue;
             NamedNodeMap metaAttr = item.getAttributes();
 
-            if(metaAttr.getNamedItem("android:name").getNodeValue().equals("WIZROCKET_ACCOUNT_ID")) {
+            if (metaAttr.getNamedItem("android:name").getNodeValue().equals("WIZROCKET_ACCOUNT_ID")) {
                 m = accoundID.matcher(metaAttr.getNamedItem("android:value").getNodeValue());
-                if(m.find()) {
+                if (m.find()) {
                     logger.info("Correct format of Account ID");
                 } else {
                     logger.info("Account ID not in the correct format");
                 }
             }
 
-            if(metaAttr.getNamedItem("android:name").getNodeValue().equals("WIZROCKET_TOKEN")) {
+            if (metaAttr.getNamedItem("android:name").getNodeValue().equals("WIZROCKET_TOKEN")) {
                 m = accountToken.matcher(metaAttr.getNamedItem("android:value").getNodeValue());
-                if(m.find()) {
+                if (m.find()) {
                     logger.info("Correct format of Account Token");
                 } else {
                     logger.info("Account Token not in the correct format");
                 }
             }
 
-            if(metaAttr.getNamedItem("android:name").getNodeValue().equals("GCM_SENDER_ID")) {
+            if (metaAttr.getNamedItem("android:name").getNodeValue().equals("GCM_SENDER_ID")) {
                 String namedItem = metaAttr.getNamedItem("android:value").getNodeValue();
-                if(namedItem.startsWith("id:")) {
+                if (namedItem.startsWith("id:")) {
                     logger.info("GCM ID configured correctly");
                 } else {
                     logger.info("GCM ID configured incorrectly");
                 }
             }
 
-            if(metaAttr.getNamedItem("android:name").getNodeValue().equals("com.google.android.gms.version")) {
-                if(metaAttr.getNamedItem("android:value").getNodeValue().equals("@integer/google_play_services_version")) {
+            if (metaAttr.getNamedItem("android:name").getNodeValue().equals("com.google.android.gms.version")) {
+                if (metaAttr.getNamedItem("android:value").getNodeValue().equals("@integer/google_play_services_version")) {
                     logger.info("Meta Data name and value set correctly for GCM");
                 } else {
                     logger.info("Meta Data name and value set incorrectly for GCM");
                 }
             }
 
+            if(metaAttr.getNamedItem("android:name").getNodeValue().equals("WIZROCKET_INAPP_EXCLUDE")) {
+                if(metaAttr.getNamedItem("android:value").getNodeValue().equals("SplashActivity")) {
+                    logger.info("In-App notifications values set correctly");
+                } else {
+                    logger.info("In-App notifications values set incorrectly");
+                }
+            }
         }
     }
 
     private void validateRequiredReceiver(NodeList children) {
         int length = children.getLength();
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             Node item = children.item(i);
-            if(!item.getNodeName().equals("receiver")) continue;
+            if (!item.getNodeName().equals("receiver")) continue;
 
             NamedNodeMap receiverAttr = item.getAttributes();
 
-            if(receiverAttr.getNamedItem("android:name").getNodeValue().equals("com.wizrocket.android.sdk.InstallReferrerBroadcastReceiver")) {
-                if(receiverAttr.getNamedItem("android:exported").getNodeValue().equals("true")) {
+            if (receiverAttr.getNamedItem("android:name").getNodeValue().equals("com.wizrocket.android.sdk.InstallReferrerBroadcastReceiver")) {
+                if (receiverAttr.getNamedItem("android:exported").getNodeValue().equals("true")) {
                     logger.info("Receiver configured correctly");
-                }  else {
+                } else {
                     logger.info("Receiver configured incorrectly");
                 }
             }
 
             NodeList receiverChildren = item.getChildNodes();
             int newLength = receiverChildren.getLength();
-            for(int j = 0; j < newLength; j++) {
+            for (int j = 0; j < newLength; j++) {
                 Node childrenItem = receiverChildren.item(j);
-                if(!childrenItem.getNodeName().equals("intent-filter")) continue;
+                if (!childrenItem.getNodeName().equals("intent-filter")) continue;
                 NodeList intentChildren = childrenItem.getChildNodes();
                 int size = intentChildren.getLength();
-                for(int k = 0; k < size; k++) {
+                for (int k = 0; k < size; k++) {
                     Node childrenItem2 = intentChildren.item(k);
-                    if(!childrenItem2.getNodeName().equals("action")) continue;
+                    if (!childrenItem2.getNodeName().equals("action")) continue;
                     NamedNodeMap actionAttr = childrenItem2.getAttributes();
 
-                    if(actionAttr.getNamedItem("android:name").getNodeValue().equals("com.android.vending.INSTALL_REFERRER")) {
+                    if (actionAttr.getNamedItem("android:name").getNodeValue().equals("com.android.vending.INSTALL_REFERRER")) {
                         logger.info("Correct action class for receiver");
                     }
                 }
@@ -221,14 +229,14 @@ public class GenerateAction extends AnAction {
 
     private void validateGcmReceiver(NodeList children) {
         int length = children.getLength();
-        for(int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) {
             Node item = children.item(i);
-            if(!item.getNodeName().equals("receiver")) continue;
+            if (!item.getNodeName().equals("receiver")) continue;
 
             NamedNodeMap receiverAttr = item.getAttributes();
 
-            if(receiverAttr.getNamedItem("android:name").getNodeValue().equals("com.wizrocket.android.sdk.GcmBroadcastReceiver")) {
-                if(receiverAttr.getNamedItem("android:permission").getNodeValue().equals("com.google.android.c2dm.permission.SEND")) {
+            if (receiverAttr.getNamedItem("android:name").getNodeValue().equals("com.wizrocket.android.sdk.GcmBroadcastReceiver")) {
+                if (receiverAttr.getNamedItem("android:permission").getNodeValue().equals("com.google.android.c2dm.permission.SEND")) {
                     logger.info("GCM Receiver configured correctly");
                 } else {
                     logger.info("GCM Receiver configured incorrectly");
@@ -237,26 +245,41 @@ public class GenerateAction extends AnAction {
 
             NodeList gcmRecChildren = item.getChildNodes();
             int newLength = gcmRecChildren.getLength();
-            for(int j = 0; j < newLength; j++) {
+            for (int j = 0; j < newLength; j++) {
                 Node childrenItem = gcmRecChildren.item(j);
-                if(!childrenItem.getNodeName().equals("intent-filter")) continue;
+                if (!childrenItem.getNodeName().equals("intent-filter")) continue;
                 NodeList intentChildren = childrenItem.getChildNodes();
                 int size = intentChildren.getLength();
                 //logger.info(""+size);
-                for(int k = 0; k < size; k++) {
+                for (int k = 0; k < size; k++) {
                     //logger.info("reached here");
                     Node childrenItem2 = intentChildren.item(k);
-                    if(!(childrenItem2.getNodeName().equals("action")
-                        || childrenItem2.getNodeName().equals("category"))) continue;
+                    if (!(childrenItem2.getNodeName().equals("action")
+                            || childrenItem2.getNodeName().equals("category"))) continue;
 
                     NamedNodeMap attrMap = childrenItem2.getAttributes();
 
-                    if(attrMap.getNamedItem("android:name").getNodeValue().equals("com.google.android.c2dm.intent.RECEIVE") ||
+                    if (attrMap.getNamedItem("android:name").getNodeValue().equals("com.google.android.c2dm.intent.RECEIVE") ||
                             attrMap.getNamedItem("android:name").getNodeName().equals("com.google.android.c2dm.intent.REGISTRATION") ||
                             attrMap.getNamedItem("android:name").getNodeName().equals(userPackage)) {
                         logger.info("Correct action and category tags configured");
                     }
                 }
+            }
+        }
+    }
+
+    private void validateInAppNotifications(NodeList children) {
+        int length = children.getLength();
+        for (int i = 0; i < length; i++) {
+            Node item = children.item(i);
+            if (!item.getNodeName().equals("activity")) continue;
+
+            NamedNodeMap actionAttr = item.getAttributes();
+            if(actionAttr.getNamedItem("android:name").getNodeValue().equals("com.wizrocket.android.sdk.InAppNotificationActivity") && actionAttr.getNamedItem("android:theme").getNodeValue().equals("@android:style/Theme.Translucent.NoTitleBar") && actionAttr.getNamedItem("android:configChanges").getNodeValue().equals("orientation|keyboardHidden")) {
+                logger.info("In-App notifications configured correctly");
+            } else {
+                logger.info("In-App notifications configured incorrectly");
             }
         }
     }
@@ -274,3 +297,4 @@ public class GenerateAction extends AnAction {
 //        return null;
 //    }
 }
+
